34919465dfec3aca93a5c99677aca054c370e78d
Tibor Santa <maquirag@tsanta-7400.localdomain>
2020-09-22 12:14:42 +0200

    simple hash: string in, string out

--- xxoi-hash-sha3-512/func.py
+++ xxoi-hash-sha3-512/func.py
@@ -5,19 +5,11 @@
 # https://fnproject.io/
 
 import io
-import json
+from hashlib import sha3_512
 from fdk import response
 
 def handler(ctx, data: io.BytesIO = None):
-    name = "World"
-    try:
-        body = json.loads(data.getvalue())
-        name = body.get("name")
-    except (Exception, ValueError) as ex:
-        logging.getLogger().info('error parsing json payload: ' + str(ex))
-
-    return response.Response(
-        ctx, response_data=json.dumps(
-            {"message": "Hello {0}".format(name)}),
-        headers={"Content-Type": "application/json"}
-    )
+    message = data.getvalue()
+    hashed = sha3_512(message).hexdigest()
+    return response.Response(ctx, response_data=hashed)
+    
